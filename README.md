24b1e122991255e53c5fa1e1dcea7a3efb382b27
zbekxz <forestnatplay@yandex.ru>
2024-03-22 06:25:53 +0500

    develope main page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Planner "Sync"
+# "Sync" service
+![header](https://github.com/zbekxzz/sync-planner/assets/129783111/ddf6a198-d8e1-4263-8f63-c5016d98faed)
--- index.js
+++ index.js
@@ -54,6 +54,10 @@ app.get('/profile', authenticateToken, (req, res) => {
     res.sendFile("pages/profile.html");
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/pages/register.html"));
+})
+
 app.get("/login", (req, res) => {
     res.sendFile("pages/login.html");
 })
